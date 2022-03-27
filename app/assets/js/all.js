b74c895394328d368242a877490ca274f80c9990
lily <a0976198586@gmail.com>
2022-03-27 15:23:12 +0800

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,56 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+// localStorage.clear();
+const list = document.querySelector('.list');   //ul
+const sendData = document.querySelector('.send');  //送出鈕
+const textValue = document.querySelector('.text'); //輸入欄位
+const reset = document.querySelector('.reset'); // 重新設定鈕
+const data = JSON.parse(localStorage.getItem('listData')) || []; //data是陣列的名字
+
+
+sendData.addEventListener('click', addData);
+list.addEventListener('click', deleteData);
+reset.addEventListener('click', cleanAllData);
+updateList(data);
+
+
+//新增
+function addData(e){
+  e.preventDefault();
+  const txt = textValue.value;
+  if(txt==''){
+    alert('你忘了輸入內容了喔!');
+    return;
+  }
+  const todo = {
+    content: txt,
+  }
+  data.push(todo);
+  updateList(data);
+  localStorage.setItem('listData', JSON.stringify(data));
+}
+
+//更新
+function updateList(data){
+  let str = '';
+  const len = data.length;
+  for(let i=0; i<len; i++){
+    str += `<li><a href="#" data-index="${i}" class="me-2">刪除</a><span class="wordColor">${data[i].content}</span></li>`;
+  }
+  list.innerHTML = str;
+  textValue.value = '';
+}
+
+//刪除
+function deleteData(e){
+  e.preventDefault;
+  if(e.target.nodeName !== "A"){return;}
+  const index = e.target.dataset.index;
+  data.splice(index, 1);
+  localStorage.setItem('listData', JSON.stringify(data));
+  updateList(data);
+}
+
+// 重新設定
+function cleanAllData(){
+  localStorage.clear();     //清除localStorage 裡的所有資料
+  window.location.reload();  //重新刷新頁面
+}
