d550e11e51b4ec418b72622ec047466a02ae55d1
lily <a0976198586@gmail.com>
2022-03-27 15:23:31 +0800

    Update 2022-03-27T07:23:29.529Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,69 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
-});
+// localStorage.clear();
+var list = document.querySelector('.list'); //ul
+
+var sendData = document.querySelector('.send'); //送出鈕
+
+var textValue = document.querySelector('.text'); //輸入欄位
+
+var reset = document.querySelector('.reset'); // 重新設定鈕
+
+var data = JSON.parse(localStorage.getItem('listData')) || []; //data是陣列的名字
+
+sendData.addEventListener('click', addData);
+list.addEventListener('click', deleteData);
+reset.addEventListener('click', cleanAllData);
+updateList(data); //新增
+
+function addData(e) {
+  e.preventDefault();
+  var txt = textValue.value;
+
+  if (txt == '') {
+    alert('你忘了輸入內容了喔!');
+    return;
+  }
+
+  var todo = {
+    content: txt
+  };
+  data.push(todo);
+  updateList(data);
+  localStorage.setItem('listData', JSON.stringify(data));
+} //更新
+
+
+function updateList(data) {
+  var str = '';
+  var len = data.length;
+
+  for (var i = 0; i < len; i++) {
+    str += "<li><a href=\"#\" data-index=\"".concat(i, "\" class=\"me-2\">\u522A\u9664</a><span class=\"wordColor\">").concat(data[i].content, "</span></li>");
+  }
+
+  list.innerHTML = str;
+  textValue.value = '';
+} //刪除
+
+
+function deleteData(e) {
+  e.preventDefault;
+
+  if (e.target.nodeName !== "A") {
+    return;
+  }
+
+  var index = e.target.dataset.index;
+  data.splice(index, 1);
+  localStorage.setItem('listData', JSON.stringify(data));
+  updateList(data);
+} // 重新設定
+
+
+function cleanAllData() {
+  localStorage.clear(); //清除localStorage 裡的所有資料
+
+  window.location.reload(); //重新刷新頁面
+}
 //# sourceMappingURL=all.js.map
